608b4cd52fad95d8d4d327e87e8895f1c25d5531
xiaohui.lam <xiaohui.lam@icloud.com>
2018-12-18 15:58:15 +0800

    config app.key when phpunit. fix travis.ci [#165794995](https://travis-ci.com/xiaohuilam/searching/jobs/165794995)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Searching\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Searching\Providers\SearchingServiceProvider;
+use Illuminate\Encryption\Encrypter;
 
 /**
  * 测试用例抽象类
@@ -13,5 +14,17 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [SearchingServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->config->set('app.key', 'base64:' . base64_encode(Encrypter::generateKey('AES-256-CBC')));
     }
 }
