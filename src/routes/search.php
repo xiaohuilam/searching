2ccaf54daff66f4c5c201f31f2e669e702d9940a
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2020-06-01 23:58:15 +0800

    Update search.php

--- src/routes/search.php
+++ src/routes/search.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => config('search.route.prefix'), ], function () {
+Route::group(['middleware' => config('search.route.middleware', 'web'), 'prefix' => config('search.route.prefix'), ], function () {
     Route::get('searching', 'Searching\\Http\\Controllers\\SearchingController@index')->name('searching');
 });
